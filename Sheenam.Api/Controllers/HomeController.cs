e980a1d6de470b56318b03ff6b8f42b9b6a63023
GolibjonTuxtamatov <tuxtamatovgolibjon@gamil.com>
2023-09-01 15:58:54 +0500

    INFRA: Initialize API Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿//==================================================
+// CopyRight (c) Coalition of Good-Hearted Engineers
+// Free to Use to Find Comfort and Peace
+//==================================================
+
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hammasi Okey");
+    }
+}
---
+++ Sheenam.Api/Program.cs
@@ -0,0 +1,25 @@
+//==================================================
+// CopyRight (c) Coalition of Good-Hearted Engineers
+// Free to Use to Find Comfort and Peace
+//==================================================
+
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Sheenam.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                
+                    webBuilder.UseStartup<Startup>());
+        }
+    }
+}
