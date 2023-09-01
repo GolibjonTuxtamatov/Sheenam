0bac1927fa86ea85c9a6a4279c9ce21c5fc05017
GolibjonTuxtamatov <tuxtamatovgolibjon@gamil.com>
2023-09-01 17:44:35 +0500

    CODE RUB:  Add Header file

--- Sheenam.Api.Infrastructure.Build/Program.cs
+++ Sheenam.Api.Infrastructure.Build/Program.cs
@@ -1,4 +1,10 @@
-﻿using ADotNet.Clients;
+﻿//==================================================
+// CopyRight (c) Coalition of Good-Hearted Engineers
+// Free to Use to Find Comfort and Peace
+//==================================================
+
+
+using ADotNet.Clients;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
