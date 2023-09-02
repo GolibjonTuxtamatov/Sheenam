9749031032af3ee63b623742e94c544fa725d782
GolibjonTuxtamatov <tuxtamatovgolibjon@gamil.com>
2023-09-02 15:41:28 +0500

    DATA: Add New Migrate

--- Sheenam.Api/Models/Foundations/Guests/Guest.cs
+++ Sheenam.Api/Models/Foundations/Guests/Guest.cs
@@ -18,6 +18,9 @@ namespace Sheenam.Api.Models.Foundations.Guests
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public GenderType Gender { get; set; }
-
+        public ItemState Status { get; set; } = ItemState.Active;
+        public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.Now;
+        public DateTimeOffset UpdatedDate { get; set; }
+        public DateTimeOffset DeletedDate { get; set; }
     }
 }
