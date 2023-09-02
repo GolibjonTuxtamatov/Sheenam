357dc148d011fcbbc2b61808966562871ebcd7bf
GolibjonTuxtamatov <tuxtamatovgolibjon@gamil.com>
2023-09-02 08:30:03 +0500

    CODE RUB: Add Header

--- Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
@@ -1,6 +1,16 @@
-﻿namespace Sheenam.Api.Brokers.Storages
+﻿//==================================================
+// CopyRight (c) Coalition of Good-Hearted Engineers
+// Free to Use to Find Comfort and Peace
+//==================================================
+
+
+using Sheenam.Api.Models.Foundations.Guests;
+using System.Threading.Tasks;
+
+namespace Sheenam.Api.Brokers.Storages
 {
-    public interface IStorageBroker
+    public partial interface IStorageBroker
     {
+        ValueTask<Guest> InsertGuestAsync(Guest guest);
     }
 }
--- Sheenam.Api/Brokers/Storages/IStorageBroker.cs
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.cs
@@ -1,6 +1,11 @@
-﻿namespace Sheenam.Api.Brokers.Storages
+﻿//==================================================
+// CopyRight (c) Coalition of Good-Hearted Engineers
+// Free to Use to Find Comfort and Peace
+//==================================================
+
+
+namespace Sheenam.Api.Brokers.Storages
 {
-    public interface IStorageBroker
-    {
-    }
+    public partial interface IStorageBroker
+    { }
 }
