275c4783e2b64d614be31c45bb26a2b1f219f721
GolibjonTuxtamatov <tuxtamatovgolibjon@gamil.com>
2023-09-26 19:26:07 +0500

    CODE RUB: Add header to some files

--- Sheenam.Api/Controllers/GuestsController.cs
+++ Sheenam.Api/Controllers/GuestsController.cs
@@ -5,7 +5,6 @@
 
 
 using System;
-using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
--- Sheenam.Api/Models/Foundations/Hosts/ItemState.cs
+++ Sheenam.Api/Models/Foundations/Hosts/ItemState.cs
@@ -1,4 +1,10 @@
-﻿namespace Sheenam.Api.Models.Foundations.Hosts
+﻿//==================================================
+// CopyRight (c) Coalition of Good-Hearted Engineers
+// Free to Use to Find Comfort and Peace
+//==================================================
+
+
+namespace Sheenam.Api.Models.Foundations.Hosts
 {
     public enum ItemState
     {
