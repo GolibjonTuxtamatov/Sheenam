df3c995a1f29a66b327740738f688fa9241cfd3c
Golibjon <113083394+GolibjonTuxtamatov@users.noreply.github.com>
2024-07-21 21:54:26 +0500

    ShouldThrowServiceExceptionOnModifyIfServiceErrorOccursAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foudations/Guests/GuestServiceTests.Exceptions.Modify.cs
+++ Sheenam.Api.Tests.Unit/Services/Foudations/Guests/GuestServiceTests.Exceptions.Modify.cs
@@ -55,5 +55,50 @@ namespace Sheenam.Api.Tests.Unit.Services.Foudations.Guests
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnModifyIfServiceErrorOccursAndLogItAsync()
+        {
+            //given
+            Guest someGuest = CreateRandomGuest();
+            string randomString = GetRandomString();
+
+            var exception = new Exception(randomString);
+
+            var failedServiceException = new FailedServiceException(exception);
+
+            var expectedGuestServiceException = new GuestServiceException(failedServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGuestByIdAsync(someGuest.Id))
+                    .ReturnsAsync(someGuest);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.UpdateGuestAsync(someGuest))
+                .ThrowsAsync(exception);
+
+            //when
+            ValueTask<Guest> addGuestTask =
+                this.guestServic.ModifyGuestAsync(someGuest);
+
+            //then
+            await Assert.ThrowsAsync<GuestServiceException>(
+                addGuestTask.AsTask);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuestByIdAsync(someGuest.Id),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateGuestAsync(someGuest),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedGuestServiceException))),
+                Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
