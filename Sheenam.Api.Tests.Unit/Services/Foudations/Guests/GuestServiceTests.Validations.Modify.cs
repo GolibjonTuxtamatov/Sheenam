b37cba965042c3b0548a6bc20330925338040ad8
Golibjon <113083394+GolibjonTuxtamatov@users.noreply.github.com>
2024-07-21 18:30:44 +0500

    ShouldThrowValidationExcetionOnModifyIfGuestDoesntExistAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foudations/Guests/GuestServiceTests.Validations.Modify.cs
+++ Sheenam.Api.Tests.Unit/Services/Foudations/Guests/GuestServiceTests.Validations.Modify.cs
@@ -127,5 +127,39 @@ namespace Sheenam.Api.Tests.Unit.Services.Foudations.Guests
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExcetionOnModifyIfGuestDoesntExistAndLogItAsync()
+        {
+            //given
+            Guest notExistGuest = CreateRandomGuest();
+            Guest nullGuest = null;
+
+            var notFoundGuetException = new NotFoundGuestException(notExistGuest.Id);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGuestByIdAsync(notExistGuest.Id))
+                    .ReturnsAsync(nullGuest);
+
+            var expectedGuestValidationException =
+                new GuestValidationException(notFoundGuetException);
+
+            //when
+            ValueTask<Guest> retrieveGuestyIdTask =
+                this.guestServic.ModifyGuestAsync(notExistGuest);
+
+            await Assert.ThrowsAsync<GuestValidationException>(retrieveGuestyIdTask.AsTask);
+
+            //then
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuestByIdAsync(notExistGuest.Id), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedGuestValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
